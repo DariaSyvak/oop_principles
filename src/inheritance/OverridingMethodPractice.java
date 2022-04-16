51759267435b7cbd8ba4f376ec78cef2e2c6975c
DariaSyvak <darina1994dsa@gmail.com>
2022-04-16 15:41:42 -0500

    overriding introduction

---
+++ src/inheritance/OverridingMethodPractice.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class OverridingMethodPractice {
+    public static void main(String[] args) {
+        Tester tester=new Tester();
+
+        tester.eat();
+        tester.sleep();
+        tester.walk();
+        tester.testing();
+        tester.learn();
+    }
+}
--- src/inheritance/Tester.java
+++ src/inheritance/Tester.java
@@ -1,5 +1,7 @@
 package inheritance;
 
+import com.oracle.xmlns.internal.webservices.jaxws_databinding.SoapBindingParameterStyle;
+
 public class Tester extends Person {
     public Tester(){
 
@@ -9,5 +11,25 @@ public class Tester extends Person {
     }
     public void testing(){
         System.out.println("Tester testing");
+
+    }
+    public void eat(){
+        System.out.println("Tester eats");
+
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Tester sleeps");
+    }
+
+    @Override
+    public void learn() {
+        System.out.println("Tester learns");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Tester walks");
     }
 }
